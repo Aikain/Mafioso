cf557b506e0b2c4a325a5e2b9e6766b44c11d671
Ville Nupponen <ville.nupponen@aika.in>
2023-07-25 07:52:21 +0300

    Implement home screen

--- app/src/main/java/app/mafioso/MafiosoScreen.kt
+++ app/src/main/java/app/mafioso/MafiosoScreen.kt
@@ -5,11 +5,15 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import app.mafioso.ui.CreateGameScreen
 import app.mafioso.ui.GameScreen
 import app.mafioso.ui.HomeScreen
+import app.mafioso.ui.JoinGameScreen
 
 enum class MafiosoScreen() {
     HOME,
+    CREATE_GAME,
+    JOIN_GAME,
     GAME,
 }
 
@@ -22,7 +26,16 @@ fun MafiosoApp(
         startDestination = MafiosoScreen.HOME.name,
     ) {
         composable(route = MafiosoScreen.HOME.name) {
-            HomeScreen()
+            HomeScreen(
+                goToCreateGameScreen = { navController.navigate(MafiosoScreen.CREATE_GAME.name) },
+                goToJoinGameScreen = { navController.navigate(MafiosoScreen.JOIN_GAME.name) },
+            )
+        }
+        composable(route = MafiosoScreen.CREATE_GAME.name) {
+            CreateGameScreen()
+        }
+        composable(route = MafiosoScreen.JOIN_GAME.name) {
+            JoinGameScreen()
         }
         composable(route = MafiosoScreen.GAME.name) {
             GameScreen()
--- app/src/main/java/app/mafioso/ui/HomeScreen.kt
+++ app/src/main/java/app/mafioso/ui/HomeScreen.kt
@@ -1,15 +1,60 @@
 package app.mafioso.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.mafioso.R
 
 @Composable
 fun HomeScreen(
+    goToCreateGameScreen: () -> Unit,
+    goToJoinGameScreen: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = "HomeScreen",
+    Column(
         modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = goToCreateGameScreen,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = stringResource(R.string.create_game_btn),
+                modifier = modifier,
+            )
+        }
+        Button(
+            onClick = goToJoinGameScreen,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = stringResource(R.string.join_game_btn),
+                modifier = modifier,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(
+        goToCreateGameScreen = {},
+        goToJoinGameScreen = {},
     )
 }
